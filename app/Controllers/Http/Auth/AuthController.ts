bed8a3dcb34c5dcad6b0360839ba11853a633418
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2020-11-14 11:34:03 -0300

    feat: add try catch on AuthController.login

--- app/Controllers/Http/Auth/AuthController.ts
+++ app/Controllers/Http/Auth/AuthController.ts
@@ -4,15 +4,20 @@ import User from 'App/Models/User'
 
 export default class AuthController {
   public async login({ response, request, auth }: HttpContextContract) {
-    const { uid, password } = request.only(['uid', 'password'])
+    try {
+      const { uid, password } = request.only(['uid', 'password'])
 
-    const token = await auth.attempt(uid, password, { expiresIn: '10 days' })
-    const user = auth.user as User
-    const profile = await user.related('profile').query().first()
-    const { username, email } = user
-    const { avatar, firstName, surname } = profile as Profile
+      const token = await auth.attempt(uid, password, { expiresIn: '10 days' })
+      const user = auth.user as User
+      const profile = await user.related('profile').query().first()
+      const { username, email } = user
+      const { avatar, firstName, surname } = profile as Profile
 
-    return response.send({ token, user: { email, username, avatar, firstName, surname } })
+      return response.send({ token, user: { email, username, avatar, firstName, surname } })
+    } catch (_) {
+      const message = 'Credenciais inválidas.'
+      return response.status(401).send({ message })
+    }
   }
 
   public async logout({ response, auth }: HttpContextContract) {
