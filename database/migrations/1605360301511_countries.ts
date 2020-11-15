493119b6aeb4ff2389722766aa4bd49edbab6e24
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2020-11-15 08:04:23 -0300

    feat: add countries table

---
+++ database/migrations/1605360301511_countries.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Countries extends BaseSchema {
+  protected tableName = 'countries'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.boolean('status').defaultTo(true)
+      table.timestamps(true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
